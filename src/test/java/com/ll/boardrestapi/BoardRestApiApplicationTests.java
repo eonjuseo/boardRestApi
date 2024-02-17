e67560620ae4c9e5852952bc9bec747d1d426f8a
eonjuseo <ejooss@chungbuk.ac.kr>
2024-02-17 16:13:31 +0900

    init

---
+++ src/test/java/com/ll/boardrestapi/BoardRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.boardrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
