1e324b30fb3f7cf4b0ed5a055d3c67e3fa06ef8c
eonjuseo <ejooss@chungbuk.ac.kr>
2024-02-20 16:50:45 +0900

    feat: 게시글 상세 BoardController 구현

--- src/main/java/com/ll/boardrestapi/domain/board/controller/BoardController.java
+++ src/main/java/com/ll/boardrestapi/domain/board/controller/BoardController.java
@@ -6,10 +6,7 @@ import com.ll.boardrestapi.domain.board.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RequestMapping("/board")
@@ -17,6 +14,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class BoardController {
 
     private final BoardService boardService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<BoardResponse> readBoard(@PathVariable long id) {
+        return new ResponseEntity<>(boardService.findById(id), HttpStatus.OK);
+    }
+
     @PostMapping("/create")
     public ResponseEntity<BoardResponse> createBoard(@RequestBody BoardRequest boardRequest) {
 
