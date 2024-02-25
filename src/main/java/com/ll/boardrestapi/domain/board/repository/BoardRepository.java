db5d1d4e96aa186389e56635ec32dba8d5be9711
eonjuseo <ejooss@chungbuk.ac.kr>
2024-02-26 02:33:39 +0900

    feat: 게시판 공개 상태 BoardRepository 구현

--- src/main/java/com/ll/boardrestapi/domain/board/repository/BoardRepository.java
+++ src/main/java/com/ll/boardrestapi/domain/board/repository/BoardRepository.java
@@ -1,7 +1,20 @@
 package com.ll.boardrestapi.domain.board.repository;
 
+import com.ll.boardrestapi.domain.board.dto.BoardResponse;
 import com.ll.boardrestapi.domain.board.entity.Board;
+import com.ll.boardrestapi.domain.board.entity.BoardStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
+
+//    @Query("SELECT b.id, b.title, b.content, b.member.id, b.member.name " +
+//            "FROM Board b " +
+//            "WHERE b.boardStatus = :boardStatus")
+//    List<Board> findByBoardStatus(@Param("boardStatus") BoardStatus boardStatus);
+
+    List<Board> findByBoardStatus(BoardStatus boardStatus);
 }
